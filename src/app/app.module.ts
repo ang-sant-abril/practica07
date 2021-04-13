a3ff7b3d4de1dbde260d012af00dbbe8dbc4c833
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-13 10:25:35 +0000

    ends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DirectivaNgForComponent } from './directiva-ng-for/directiva-ng-for.component';
+import { DirectivaNgIfComponent } from './directiva-ng-if/directiva-ng-if.component';
+import { DirectivaNgSwitchComponent } from './directiva-ng-switch/directiva-ng-switch.component';
+import { DirectivaNgStyleComponent } from './directiva-ng-style/directiva-ng-style.component';
+import { DirectivaNgClassComponent } from './directiva-ng-class/directiva-ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivaNgForComponent
+    DirectivaNgForComponent,
+    DirectivaNgIfComponent,
+    DirectivaNgSwitchComponent,
+    DirectivaNgStyleComponent,
+    DirectivaNgClassComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
